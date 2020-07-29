842dd6eca8375c0e95b52ef59d28f24c5cafa758
KUMAR ROUNAK <rounaksinghrajput777@gmail.com>
2020-07-29 22:25:17 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
-mkdir -p ~/.streamlit/                                               
-echo "\                       
-[server]\n\                       
-port = $PORT\n\                       
-enableCORS = false\n\                       
-headless = true\n\                       
-\n\                       
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"rounaksinghrajput777@gmail.com.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
